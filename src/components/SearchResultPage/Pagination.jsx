6b5b1548758177562b9b902020131a3fd77f1c67
pyyhekumi82 <pyyhekumi82@gmail.com>
2024-03-12 19:53:43 +0900

    dddd
    dddd

--- src/components/SearchResultPage/Pagination.jsx
+++ src/components/SearchResultPage/Pagination.jsx
@@ -1,17 +1,72 @@
-// src/components/SearchResultPage/Pagination.jsx
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleLeft, FaAngleLeft, FaAngleRight, FaAngleDoubleRight } from 'react-icons/fa';
+import styled from 'styled-components';
 
-import React from 'react';
+const PaginationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-const Pagination = (props) => {
-    return (
-        <div style={{ marginTop: '20px', textAlign: 'center' }}>
-            {props.pageNumbers.map((number) => (
-                <span key={number} style={{ margin: '15px', cursor: 'pointer' }} onClick={() => props.handlePageClick(number)}>
-                    {number}
-                </span>
-            ))}
-        </div>
-    );
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const PageButton = styled.span`
+  margin: 0.5rem;
+  cursor: pointer;
+  ${({ ariaCurrent }) => ariaCurrent && 'font-weight: bold;'}
+`;
+
+const sliceArrayByLimit = (totalPage, limit) => {
+  const array = [];
+  for (let i = 0; i < totalPage; i += limit) {
+    array.push(Array.from({ length: limit }, (_, index) => i + index));
+  }
+  return array;
+};
+
+const Pagination = ({ totalPage, limit, page, setPage }) => {
+  // 총 페이지 갯수에 따라 Pagination 갯수 정하기, limit 단위로 페이지 리스트 넘기기
+  const [currentPageArray, setCurrentPageArray] = useState([]);
+  const [totalPageArray, setTotalPageArray] = useState([]);
+
+  useEffect(() => {
+    if (page % limit === 1) {
+      setCurrentPageArray(totalPageArray[Math.floor(page / limit)]);
+    } else if (page % limit === 0) {
+      setCurrentPageArray(totalPageArray[Math.floor(page / limit) - 1]);
+    }
+  }, [page]);
+
+  useEffect(() => {
+    const slicedPageArray = sliceArrayByLimit(totalPage, limit);
+    setTotalPageArray(slicedPageArray);
+    setCurrentPageArray(slicedPageArray[0]);
+  }, [totalPage]);
+
+  return (
+    <PaginationWrapper>
+      <FaAngleDoubleLeft onClick={() => setPage(1)} disabled={page === 1} />
+      <FaAngleLeft onClick={() => setPage(page - 1)} disabled={page === 1} />
+      <ButtonWrapper>
+        {currentPageArray?.map((array) =>
+          array.map((i) => (
+            <PageButton
+              key={i + 1}
+              onClick={() => setPage(i + 1)}
+              ariaCurrent={page === i + 1 ? 'page' : null}
+            >
+              {i + 1}
+            </PageButton>
+          ))
+        )}
+      </ButtonWrapper>
+      <FaAngleRight onClick={() => setPage(page + 1)} disabled={page === totalPage} />
+      <FaAngleDoubleRight onClick={() => setPage(totalPage)} disabled={page === totalPage} />
+    </PaginationWrapper>
+  );
 };
 
 export default Pagination;
--- src/components/SearchResultPage/SearchResultList.jsx
+++ src/components/SearchResultPage/SearchResultList.jsx
@@ -1,12 +1,10 @@
-// src/components/SearchResultPage/SearchResultList.jsx
-
 import React from 'react';
 
 const SearchResultList = (props) => {
     return (
-        <div style={{ maxHeight: '600px', overflowY: 'auto', border: '1px solid black', padding: '10px' }}>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
             {props.searchResults.map((item, index) => (
-                <div key={index} style={{ border: '1px solid black', margin: '5px', padding: '5px', width: '80%', display: 'inline-block' }}>
+                <div key={index} style={{ border: '1px solid #92baff', margin: '5px', padding: '5px', width: '100%', backgroundColor: '#fff' }}>
                     {item.title} - {item.description}
                 </div>
             ))}
