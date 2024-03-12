6678c8f607bd23059fbd06748150af2a32f08552
pyyhekumi82 <pyyhekumi82@gmail.com>
2024-03-12 20:32:23 +0900

    Update Pagination.jsx

--- src/components/SearchResultPage/Pagination.jsx
+++ src/components/SearchResultPage/Pagination.jsx
@@ -1,4 +1,3 @@
-// src/components/SearchResultPage/Pagination.jsx
 import React, { useState, useEffect } from 'react';
 import { FaAngleDoubleLeft, FaAngleLeft, FaAngleRight, FaAngleDoubleRight } from 'react-icons/fa';
 import styled from 'styled-components';
@@ -20,49 +19,37 @@ const PageButton = styled.span`
   ${({ ariaCurrent }) => ariaCurrent && 'font-weight: bold;'}
 `;
 
-const sliceArrayByLimit = (totalPage, limit) => {
-  const array = [];
-  for (let i = 0; i < totalPage; i += limit) {
-    array.push(Array.from({ length: limit }, (_, index) => i + index));
-  }
-  return array;
-};
-
 const Pagination = ({ totalPage, limit, page, setPage }) => {
   // 총 페이지 갯수에 따라 Pagination 갯수 정하기, limit 단위로 페이지 리스트 넘기기
   const [currentPageArray, setCurrentPageArray] = useState([]);
-  const [totalPageArray, setTotalPageArray] = useState([]);
-
-  useEffect(() => {
-    if (page % limit === 1) {
-      setCurrentPageArray(totalPageArray[Math.floor(page / limit)]);
-    } else if (page % limit === 0) {
-      setCurrentPageArray(totalPageArray[Math.floor(page / limit) - 1]);
-    }
-  }, [page]);
 
   useEffect(() => {
     const slicedPageArray = sliceArrayByLimit(totalPage, limit);
-    setTotalPageArray(slicedPageArray);
-    setCurrentPageArray(slicedPageArray[0]);
-  }, [totalPage]);
+    setCurrentPageArray(slicedPageArray);
+  }, [totalPage, limit]);
+
+  const sliceArrayByLimit = (totalPage, limit) => {
+    const array = [];
+    for (let i = 0; i < totalPage; i += limit) {
+      array.push(i + 1); // 페이지 번호를 배열에 추가합니다.
+    }
+    return array;
+  };
 
   return (
     <PaginationWrapper>
       <FaAngleDoubleLeft onClick={() => setPage(1)} disabled={page === 1} />
       <FaAngleLeft onClick={() => setPage(page - 1)} disabled={page === 1} />
       <ButtonWrapper>
-        {currentPageArray?.map((array) =>
-          array.map((i) => (
-            <PageButton
-              key={i + 1}
-              onClick={() => setPage(i + 1)}
-              ariaCurrent={page === i + 1 ? 'page' : null}
-            >
-              {i + 1}
-            </PageButton>
-          ))
-        )}
+        {currentPageArray.map((pageNumber) => (
+          <PageButton
+            key={pageNumber}
+            onClick={() => setPage(pageNumber)}
+            ariaCurrent={page === pageNumber ? 'page' : null}
+          >
+            {pageNumber}
+          </PageButton>
+        ))}
       </ButtonWrapper>
       <FaAngleRight onClick={() => setPage(page + 1)} disabled={page === totalPage} />
       <FaAngleDoubleRight onClick={() => setPage(totalPage)} disabled={page === totalPage} />
