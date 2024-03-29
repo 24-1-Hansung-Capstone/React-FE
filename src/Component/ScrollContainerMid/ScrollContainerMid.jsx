cd07f4508801387bfca6ed87232460773d95ff46
lee <sjlee9908@naver.com>
2024-03-29 18:24:00 +0900

    크아아앙악

--- src/Component/ScrollContainerMid/ScrollContainerMid.jsx
+++ src/Component/ScrollContainerMid/ScrollContainerMid.jsx
@@ -6,7 +6,6 @@ function ScrollContainerMid(props){
         <div style={{
             display: 'flex', // Flexbox 모델을 사용합니다.
             justifyContent: 'center', // 주 축(여기서는 수평 축)을 기준으로 중앙 정렬합니다.
-            width: '100%', // 부모 컨테이너의 폭을 최대로 설정합니다.
         }}>
             <ScrollContainer></ScrollContainer>
         </div>
