9d8273e4e2a43bfc1bed6110e0ddab3c52a2c78a
pyyhekumi82 <pyyhekumi82@gmail.com>
2024-04-26 15:52:59 +0900

    nfkdn
    fdsafdsa

--- src/index.js
+++ src/index.js
@@ -11,10 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
-// Dynamically add Kakao Maps SDK script to the document
-const kakaoScript = document.createElement('script');
-kakaoScript.src = '//dapi.kakao.com/v2/maps/sdk.js?appkey=850a9218cb545efb10a9bbc88723f254&libraries=services,clusterer&autoload=false';
-kakaoScript.async = true;
-document.body.appendChild(kakaoScript);
-
 reportWebVitals();
