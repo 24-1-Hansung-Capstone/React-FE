44a19b552b657d8df36e22c2d8fbbc5fd3f03479
sjlee9908 <108540909+sjlee9908@users.noreply.github.com>
2024-04-27 18:19:38 +0900

    Update RelatedSearchWordsStyle.jsx

--- src/Component/SearchResultPage/style/RelatedSearchWordsStyle.jsx
+++ src/Component/SearchResultPage/style/RelatedSearchWordsStyle.jsx
@@ -2,6 +2,7 @@ import RelatedSearchWords from "../RelatedSearchWords";
 
 const style = {
     relatedSearchWords :{
+        marginLeft: "120px",
         display : "flex",
         flexDirection: "row",
         flexWrap: "wrap",
