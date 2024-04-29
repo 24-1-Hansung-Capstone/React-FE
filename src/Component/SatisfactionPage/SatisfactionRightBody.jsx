33946cdb6d9c55df5d5db7433f4019834ec631c2
pyyhekumi82 <pyyhekumi82@gmail.com>
2024-04-29 19:00:15 +0900

    매물만족도 지도추가

--- src/Component/SatisfactionPage/SatisfactionRightBody.jsx
+++ src/Component/SatisfactionPage/SatisfactionRightBody.jsx
@@ -1,12 +1,156 @@
-import React from "react";
+//srr\Component\SatisfactionPage\SatisfactionRightBody.jsx
+import React, { useEffect, useState } from 'react';
+import { MapContainer, InputForm, ResultList, Pagination, SearchButton, Wrapper, CurrentPosButton } from "./style"; // CurrentPosButton import 추가
+import searchIcon from "../../Asset/searchIcon.svg"; // 이미지 불러오기
 
-function SatisfactionRightBody(){
-    return (
-        <div>
-            <div>지도</div>
-            <div>장소 설명</div>
+const { kakao } = window;
+
+const SatisfactionRightBody = () => {
+  const [InputText, setInputText] = useState('');
+  const [Places, setPlaces] = useState([]);
+  
+  useEffect(() => {
+    kakao.maps.load(() => {
+      const container = document.getElementById('myMap');
+      const options = {
+        center: new kakao.maps.LatLng(37.566826, 126.9786567),
+        level: 3,
+      };
+      const map = new kakao.maps.Map(container, options);
+      const ps = new kakao.maps.services.Places();
+      var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+      let marker; // marker 정의
+
+      ps.keywordSearch(InputText, placesSearchCB, { size: 5 });
+
+      function placesSearchCB(data, status, pagination) {
+        if (status === kakao.maps.services.Status.OK) {
+          let bounds = new kakao.maps.LatLngBounds();
+
+          for (let i = 0; i < data.length; i++) {
+            displayMarker(data[i]);
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+          }
+
+          map.setBounds(bounds);
+          displayPagination(pagination);
+          setPlaces(data);
+        }
+      }
+
+      function displayPagination(pagination) {
+        var paginationEl = document.getElementById('pagination'),
+          fragment = document.createDocumentFragment(),
+          i;
+
+        while (paginationEl.hasChildNodes()) {
+          paginationEl.removeChild(paginationEl.lastChild);
+        }
+
+        for (i = 1; i <= pagination.last; i++) {
+          var el = document.createElement('a');
+          el.href = '#';
+          el.innerHTML = i;
+
+          if (i === pagination.current) {
+            el.className = 'on';
+          } else {
+            el.onclick = (function (i) {
+              return function () {
+                pagination.gotoPage(i);
+              };
+            })(i);
+          }
+
+          fragment.appendChild(el);
+        }
+        paginationEl.appendChild(fragment);
+      }
+
+      function displayMarker(place) {
+        marker = new kakao.maps.Marker({ // marker 변수 선언
+          map: map,
+          position: new kakao.maps.LatLng(place.y, place.x),
+        });
+
+        kakao.maps.event.addListener(marker, 'click', function () {
+          infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+          infowindow.open(map, marker);
+        });
+      }
+    });
+  }, [InputText]);
+
+  const onChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const getCurrentPosBtn = () => { // getCurrentPosBtn 함수 정의
+    navigator.geolocation.getCurrentPosition(
+      getPosSuccess,
+      () => alert("위치 정보를 가져오는데 실패했습니다."),
+      {
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+        timeout: 27000,
+      }
+    );
+  }
+  
+  const getPosSuccess = (pos) => {
+    const container = document.getElementById('myMap');
+    const options = {
+      center: new kakao.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
+    const marker = new kakao.maps.Marker({
+      map: map,
+      position: new kakao.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+    });
+  };
+
+  const onSearchClick = () => {
+    // 검색 버튼 클릭 시 실행할 함수
+    // 여기에 기존의 검색 기능 구현
+  };
+
+  return (
+    <Wrapper>
+      <div style={{ display: 'flex', position: 'relative' }}>
+        <div style={{ flex: 1 }}>
+          <InputForm onSubmit={(e) => e.preventDefault()}>
+            <input placeholder="검색어를 입력하세요" onChange={onChange} value={InputText} />
+            <SearchButton type="button" onClick={onSearchClick}>
+              <img src={searchIcon} alt="Search" /> {/* 이미지 아이콘 추가 */}
+            </SearchButton>
+          </InputForm>
+          <ResultList style={{ display: InputText ? 'block' : 'none' }}>
+            {Places.map((item, i) => (
+              <div key={i}>
+                <span>{i + 1}</span>
+                <div>
+                  <h5>{item.place_name}</h5>
+                  {item.road_address_name ? (
+                    <div>
+                      <span>{item.road_address_name}</span>
+                      <span>{item.address_name}</span>
+                    </div>
+                  ) : (
+                    <span>{item.address_name}</span>
+                  )}
+                  <span>{item.phone}</span>
+                </div>
+              </div>
+            ))}
+            <Pagination id="pagination"></Pagination>
+          </ResultList>
+          <CurrentPosButton onClick={getCurrentPosBtn}>현재 위치</CurrentPosButton> {/* 수정된 버튼 */}
         </div>
-    )
+        <MapContainer id="myMap"></MapContainer>
+      </div>
+    </Wrapper>
+  );
 }
 
-export default SatisfactionRightBody
+export default SatisfactionRightBody;
---
+++ src/Component/SatisfactionPage/style.js
@@ -0,0 +1,127 @@
+//srr\Component\MapPage\SatisfactionPage.js
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  position: relative;
+  width: 50%;
+`;
+
+export const MapContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 50vh;
+  margin: 0 0;
+  margin-top: 5px;
+`;
+
+export const InputForm = styled.form`
+  margin-top: 20px;
+  margin-left: 50px;
+  width: 50%;
+  max-width: 468px;
+  border-radius: 40px;
+  display: inline-flex;
+  border: 2px solid #92baff;
+  position: absolute;
+  z-index: 2;
+  background-color: white;
+
+  input {
+    border: none;
+    outline: none;
+    margin-left: 20px;
+    background-color: white;
+    flex: 1;
+  }
+`;
+
+export const ResultList = styled.div`
+  position: absolute;
+  top: 130px;
+  left: 0;
+  margin-left: 40px;
+  background-color: rgba(255, 255, 255);
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  z-index: 2;
+  font-size: 13px;
+  max-height: calc(82% - 10px);
+  max-width: 468px;
+  width: calc(30% - 10px);
+  overflow-y: auto;
+  border-radius: 10px;
+
+  div {
+    margin-top: 5px;
+    padding: 5px;
+    background-color: #f5f5f5; /* Added background color for inner divs */
+    border-radius: 5px; /* Added border radius for inner divs */
+  }
+
+  h5 {
+    font-size: 18px; /* Increased font size */
+    margin-bottom: 2px; /* Added margin bottom */
+    color: #333; /* Changed text color */
+  }
+
+  span {
+    display: block; /* Changed display to block */
+    margin-bottom: 2px; /* Added margin bottom */
+    color: #666; /* Changed text color */
+  }
+`;
+
+export const Pagination = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+
+  a {
+    margin: 0 5px;
+    padding: 5px 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    text-decoration: none;
+    color: #333;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+      background-color: #f0f0f0;
+    }
+
+    &.on {
+      background-color: #92baff;
+      color: #fff;
+      cursor: default;
+    }
+  }
+`;
+
+export const SearchButton = styled.button`
+  display: inline-block;
+  width: 5%;
+  margin: 10px;
+  background: none;
+  border: none;
+  padding: 0;
+  cursor: pointer;
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+`;
+
+export const CurrentPosButton = styled.button`
+  position: absolute;
+  bottom: 20px; /* 조정 가능한 버튼 위치 */
+  right: 20px; /* 조정 가능한 버튼 위치 */
+  background-color: #92baff;
+  color: #fff;
+  border: none;
+  border-radius: 10px;
+  padding: 7px 7px;
+  font-size: 16px;
+  cursor: pointer;
+  z-index: 2;
+`;
