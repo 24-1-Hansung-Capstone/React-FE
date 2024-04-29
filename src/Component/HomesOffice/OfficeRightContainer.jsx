2658e5c0929fadebc843b71e4f99808ed8a279b4
jin3811 <hjwoo1028@naver.com>
2024-04-29 18:56:50 +0900

    홈즈 사무소 배치 1차 완성

--- src/Component/HomesOffice/OfficeRightContainer.jsx
+++ src/Component/HomesOffice/OfficeRightContainer.jsx
@@ -2,14 +2,18 @@ import React, {} from "react";
 
 const OfficeRightContainer = ({ data, selectPostId }) => {
 
-    const selectPost = data.find(({id}) => id == selectPostId)
+    const selectPost = data.find(({id}) => id === selectPostId)
 
     return (
-        <>
+        <div className="officePost">
             {
                 selectPost != null ? (
                     <>
-                        <div> 제목 : {selectPost.name} </div><hr/>
+                        <div> 제목 : {selectPost.name} </div><br/>
+                        <div> 주소 : {selectPost.addr} </div><br/>
+                        <div> 거래유형 : {selectPost.type} </div><br/>
+                        <div> 사진 : {selectPost.img} </div><br/>
+                        <div> 내용 : {selectPost.desc} </div><br/>
                     </>
                 ) : (
                     <>
@@ -17,7 +21,7 @@ const OfficeRightContainer = ({ data, selectPostId }) => {
                     </>
                 )
             }
-        </>
+        </div>
     )
 }
 
