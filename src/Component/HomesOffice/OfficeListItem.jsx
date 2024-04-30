e096e3a5e3526eb35bb295b90cf60255bcbcacbc
pyyhekumi82 <pyyhekumi82@gmail.com>
2024-05-01 00:41:07 +0900

    추가 수정

--- src/Component/HomesOffice/OfficeListItem.jsx
+++ src/Component/HomesOffice/OfficeListItem.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import "./style/OfficeListItem.css"; // 스타일 시트 파일을 임포트합니다.
 
-const OfficeListItem = ({post:{id, type, name, addr}, setSelectPostId}) => {
-    return(
+const OfficeListItem = ({ post: { id, type, name, addr }, setSelectPostId }) => {
+    return (
         <>
-            <div>
-                <div>type : {type} <br/></div>
-                <div onClick={() => setSelectPostId(id)}>name : {name} <br/></div>
-                <div>addr : {addr} <br/></div>
+            <div className="officeListItemContainer">
+                <button className="officeListButton">{type}</button>
+                <div className="officeListItemDetails">
+                    <div onClick={() => setSelectPostId(id)}>{name}</div>
+                    <div>{addr}</div>
+                </div>
             </div>
             <br/>
         </>
