19521fc218b8f1f9a99d34ee86bbea0aba46956e
lee <sjlee9908@naver.com>
2024-05-01 03:40:30 +0900

    ㅇㅇ

--- src/Component/SatisfactionPage/SatisfactionBody.jsx
+++ src/Component/SatisfactionPage/SatisfactionBody.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import SatisfactionLeftBody from "./SatisfactionLeftBody";
 import SatisfactionRightBody from "./SatisfactionRightBody";
 import style from "./style/SatisfactionBodyStyle"
 
 function SatisfactionBody({result}) {
+    const [InputText, setInputText] = useState('');
+
     return (
         <div style={style.Body}>
-            <SatisfactionLeftBody result = {result}/>
-            <SatisfactionRightBody/>
+            <SatisfactionLeftBody result = {result} setInputText = {setInputText}/>
+            <SatisfactionRightBody InputText = {InputText} setInputText = {setInputText}/>
         </div>
     )
 }
--- src/Component/ShareFolder/SearchResultNav.jsx
+++ src/Component/ShareFolder/SearchResultNav.jsx
@@ -1,13 +1,19 @@
 // src/Components/SearchResultPage/SearchResultNav.jsx
-import React from "react";
+import React, {useEffect, useState} from "react";
 import MenuBar from "./Menubar";
 import ResultSearchInput from "./ResultSearchInput"
 import RelatedSearchWords from "../SearchResultPage/RelatedSearchWords";
 import logo from '../../Asset/Logo.png';
 import style from "./Style/SearchResultNavStyle"
+import {getSearchResult} from "./api"
 
 function SearchResultNav({ searchWord, searchUrlPath}) {
-    const recommendations = ["검색어 관련1", "검색어 관련2", "검색어 관련3", "검색어 관련4"];
+    const [recommendations, setRecommedations] = useState([]);
+
+    useEffect(()=>{
+        getSearchResult(searchWord, "relatedWord", setRecommedations)
+    }, [])
+    
 
     return (
         <div>
