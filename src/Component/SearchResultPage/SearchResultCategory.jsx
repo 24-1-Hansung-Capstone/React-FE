79f75dc2ff18def2d2afce67b814241a25addd89
lee <sjlee9908@naver.com>
2024-05-03 19:27:00 +0900

    dd

--- src/Component/SearchResultPage/SearchResultCategory.jsx
+++ src/Component/SearchResultPage/SearchResultCategory.jsx
@@ -9,36 +9,32 @@ import { RiCriminalLine } from "react-icons/ri";
 import { TbBuildingEstate } from "react-icons/tb";
 import SearchResultCategoryHeader from "./SearchResultCategoryHeader";
 import style from "./style/SearchResultCategoryStyle"
+import { useNavigate } from "react-router-dom";
 
 export default function SearchResultCategory({ searchResults, context }) {
     const [isOpen, setIsOpen] = useState(true); // 검색결과 아이템을 띄울 것인가?
     const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
     const totalItem = 5; // 페이지에 띄울 아이템 개수
-    const totalPages = parseInt(searchResults.length / totalItem ) + (searchResults.length % totalItem !== 0 ? 1 : 0); // 전체 페이지 개수
+    const totalPages = parseInt(searchResults.length / totalItem) + (searchResults.length % totalItem !== 0 ? 1 : 0); // 전체 페이지 개수
 
     const handleToggle = () => {
         setIsOpen(!isOpen);
     };
 
-    // console.log(`currentPage : ${currentPage} \nsetPage : ${setPage} \ntotalItem : ${searchResults.length} \ntotalPage : ${totalPages}`)
-
-    // const handlePageChange = (pageNumber) => {
-    //     setCurrentPage(pageNumber);
-    // };
 
     const displayItem = searchResults.slice(5 * (currentPage - 1),
-                                            currentPage === totalPages  ? undefined // 맨 마지막 페잊라면, 마지막 데이터까지 자른다.
-                                                                        : 5 * currentPage) // 중간 페이지라면, setpage만큼 띄운다 
+        currentPage === totalPages ? undefined // 맨 마지막 페잊라면, 마지막 데이터까지 자른다.
+            : 5 * currentPage) // 중간 페이지라면, setpage만큼 띄운다 
 
     return (
-        <div style = {style.searchResultCategoryStyle}>
+        <div style={style.searchResultCategoryStyle}>
             <SearchResultCategoryHeader context={context} isOpen={isOpen} handleToggle={handleToggle} />
             {isOpen && (
                 <div>
                     {
                         displayItem.map((item, index) => {
-                            return(
-                                <HyperlinkPreview id={index} title={item["esDto"]["title"]} itemPreview = {item["preview"]} url={item["esDto"]["url"]} />
+                            return (
+                                <HyperlinkPreview id={index} title={item["esDto"]["title"]} itemPreview={item["preview"]} url={item["esDto"]["url"]} />
                             )
                         })
                     }
--- src/Component/ShareFolder/HyperlinkPreview.jsx
+++ src/Component/ShareFolder/HyperlinkPreview.jsx
@@ -1,12 +1,19 @@
 // src/Components/ShareFolder/HyperlinkPreview.jsx
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link,  } from "react-router-dom";
 import style from "./Style/HyperlinkPreviewStyle"
 
 export default function HyperlinkPreview({ title, itemPreview, url }) {
+  let target = "_blank"
+
+  if(url == null){
+    url = "/MapPage"
+    target = "_self"
+  }
+
   return (
     <div style={style.item}>
-      <Link to={url} target="_blank">
+      <Link to={url} target={target} state={{ search: title }}>
         <h1 style={style.title}>{title}</h1>
       </Link>
       <p style={style.preview}>
