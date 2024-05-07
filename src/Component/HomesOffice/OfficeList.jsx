e76edefb949ae0163400d24575431a1fa3f21501
pyyhekumi82 <pyyhekumi82@gmail.com>
2024-05-07 15:49:27 +0900

    리액트 수정

--- src/Component/HomesOffice/OfficeList.jsx
+++ src/Component/HomesOffice/OfficeList.jsx
@@ -1,27 +1,31 @@
-// OfficeList.js
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import OfficeListItem from "./OfficeListItem";
 import { getSearchResult } from "../ShareFolder/SpringApi";
+import { officeCategory } from "./OfficeData";
 
 const OfficeList = ({ selectCategory, setSelectPostId }) => {
-    const [realtyData, setRealtyData] = useState([]);
+  const [realtyData, setRealtyData] = useState([]);
 
-    useEffect(() => {
-        getSearchResult("CommunityPage/", setRealtyData)
-    }, []); // Fetch data on component mount
+  useEffect(() => {
+    getSearchResult("CommunityPage/", setRealtyData);
+  }, []);
 
-    return (
-        <div>
-            {
-        console.log(realtyData)       }
-            {realtyData
-                .filter(({ type }) => {return selectCategory === "전체" || type === selectCategory})
-                .map(post => (
-                    <OfficeListItem key={post.id} post={post} setSelectPostId={setSelectPostId} />
-                ))}
-        </div>
-    );
+  return (
+    <div>
+      {realtyData
+        .filter(({ type }) => {
+          if (selectCategory === "전체") {
+            return true;
+          } else {
+            return type === officeCategory[selectCategory];
+          }
+        })
+        .map((post) => (
+          <OfficeListItem key={post.id} post={post} setSelectPostId={setSelectPostId} />
+        ))}
+    </div>
+  );
 };
 
 export default OfficeList;
