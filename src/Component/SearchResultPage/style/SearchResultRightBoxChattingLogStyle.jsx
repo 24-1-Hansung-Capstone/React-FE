3e0bd5c41eaea0d7faa11315917ad4162db702ab
pyyhekumi82 <pyyhekumi82@gmail.com>
2024-05-07 23:06:12 +0900

    Update SearchResultRightBoxChattingLogStyle.jsx

--- src/Component/SearchResultPage/style/SearchResultRightBoxChattingLogStyle.jsx
+++ src/Component/SearchResultPage/style/SearchResultRightBoxChattingLogStyle.jsx
@@ -17,11 +17,15 @@ const style = {
         textAlign: "left",
     },
     chattingLogs : {
+        fontSize: "17px",
         display: "flex",
         flexWrap: "nowrap",
         flexDirection: "column",
         maxHeight: "700px",
-        overflow: "auto" // 스크롤이 필요할 때만 스크롤 표시
+        overflow: "auto", // 스크롤이 필요할 때만 스크롤 표시
+        lineHeight: "2", // 폰트의 위아래 간격
+        letterSpacing: "0.03em", // 자간
+        padding: "10px", // 내부 여백
     }
     
 };
