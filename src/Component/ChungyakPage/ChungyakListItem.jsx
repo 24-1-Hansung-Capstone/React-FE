fde83138078e4db599417a7fc42a68b18776e76c
lee <sjlee9908@naver.com>
2024-05-07 18:59:30 +0900

    edd

--- src/Component/ChungyakPage/ChungyakListItem.jsx
+++ src/Component/ChungyakPage/ChungyakListItem.jsx
@@ -3,9 +3,14 @@ import { Link } from "react-router-dom";
 import style from "./style/ChangyakListItemStyle";
 
 function ChangyakListItem({house_NM, tot_SUPLY_HSHLDCO, hssply_ADRES, pblanc_URL}) {
+    function openPopup(){
+        window.open(pblanc_URL, "new", "toolbar=no, menubar=no, scrollbars=yes, resizable=no, width=700, height=700, left=0, top=0" );
+      };
+
     return (
         <div style = {style.item}>
-            <Link style ={style.link} to = {pblanc_URL} target="_blank" rel="noopener noreferrer">
+            
+            <Link style ={style.link} onClick={openPopup}>
                 <div style = {style.title}> {house_NM} </div>
                 <div style = {style.supply}>공급가구 수: {tot_SUPLY_HSHLDCO}</div>
                 <div style = {style.address}>주소: {hssply_ADRES}</div>
