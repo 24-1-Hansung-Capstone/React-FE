6ba4989c0be05be2acb547467ca49dfa2105e730
lee <sjlee9908@naver.com>
2024-05-10 17:56:53 +0900

    dd

--- src/Component/SearchResultPage/SearchResultPage.jsx
+++ src/Component/SearchResultPage/SearchResultPage.jsx
@@ -8,38 +8,11 @@ import style from "./style/SearchResultPageStyle";
 
 function SearchResultPage() {
     const [searchResults, setSearchResults] = useState([]);
-    const [summaryTitles, setSummaryTitles] = useState([]);
     const { searchTerm } = useParams();
 
     useEffect(() => {
         getSearchResult(searchTerm, "search", (res) => {
             setSearchResults(res)
-
-            const newSummaryTitles = []
-            const news = searchResults.filter(res => res.category === "news")
-            const blog = searchResults.filter(res => res.category === "blog")
-            console.log("asdfasdfasdf", news)
-            if (news.length < 2) {
-                for (let i = 0; i < news.length; i++) {
-                    newSummaryTitles.push([news[i]["esDto"]["title"], news[i]["esDto"]["url"]])
-                }
-            }
-            else {
-                for (let i = 0; i < 2; i++) {
-                    newSummaryTitles.push([news[i]["esDto"]["title"], news[i]["esDto"]["url"]])
-                }
-            }
-            if (blog.length < 2) {
-                for (let i = 0; i < blog.length; i++) {
-                    newSummaryTitles.push([blog[i]["esDto"]["title"], blog[i]["esDto"]["url"]])
-                }
-            }
-            else {
-                for (let i = 0; i < 2; i++) {
-                    newSummaryTitles.push([blog[i]["esDto"]["title"], blog[i]["esDto"]["url"]])
-                }
-            }
-            setSummaryTitles(newSummaryTitles)
         });
     }, [searchTerm]);
 
@@ -49,7 +22,7 @@ function SearchResultPage() {
                 <SearchResultNav searchWord={searchTerm} searchUrlPath={"search"} />
                 <div style={style.body}>
                     <SearchResultLeftBox searchResults={searchResults} />
-                    <SearchResultRightBox searchTerm={searchTerm} searchResults={searchResults} summaryTitles = {summaryTitles}/> {/* title props 전달 */}
+                    <SearchResultRightBox searchTerm={searchTerm} searchResults={searchResults}/> {/* title props 전달 */}
                 </div>
             </section>
         </div>
