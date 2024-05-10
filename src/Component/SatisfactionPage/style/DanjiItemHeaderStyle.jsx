d6afe81e0ebf2158ea53f51e791d9b43b32331ea
pyyhekumi82 <pyyhekumi82@gmail.com>
2024-05-10 17:49:41 +0900

    style 수정

--- src/Component/SatisfactionPage/style/DanjiItemHeaderStyle.jsx
+++ src/Component/SatisfactionPage/style/DanjiItemHeaderStyle.jsx
@@ -7,7 +7,7 @@ const style = {
     },
     danjiName : {
         fontWeight: "bold", // 폰트를 bold로 설정
-        fontSize: "26px" // 원하는 크기로 조정
+        fontSize: "28px" // 원하는 크기로 조정
     },
     arrow : {
         width :"20px"
--- src/Component/SatisfactionPage/style/DanjiReviewStyle.jsx
+++ src/Component/SatisfactionPage/style/DanjiReviewStyle.jsx
@@ -1,7 +1,7 @@
 const style = {
     item: {
         marginTop: "25px", // camelCase로 수정
-        marginBottom: "25px",  
+          
         display: "grid", // CSS Grid로 변경
         gridTemplateColumns: "1fr 2fr", // 1개의 컬럼으로 설정
         gridGap: "20px", // 그리드 간격 설정
