5bf4cd53a14ab20d8ad91cf2e47047baf88cabbb
pyyhekumi82 <pyyhekumi82@gmail.com>
2024-05-10 16:37:56 +0900

    dfdsa

--- src/Component/SearchResultPage/SearchResultPage.jsx
+++ src/Component/SearchResultPage/SearchResultPage.jsx
@@ -8,12 +8,39 @@ import style from "./style/SearchResultPageStyle";
 
 function SearchResultPage() {
     const [searchResults, setSearchResults] = useState([]);
-    const [title, setTitle] = useState(""); // title 상태 추가
+    const [summaryTitles, setSummaryTitles] = useState([]);
     const { searchTerm } = useParams();
 
     useEffect(() => {
-        getSearchResult(searchTerm, "search", setSearchResults);
-        setTitle(searchTerm); // 검색어로 title 상태 설정
+        getSearchResult(searchTerm, "search", (res) => {
+            setSearchResults(res)
+
+            const newSummaryTitles = []
+            const news = searchResults.filter(res => res.category === "news")
+            const blog = searchResults.filter(res => res.category === "blog")
+            console.log("asdfasdfasdf", news)
+            if (news.length < 2) {
+                for (let i = 0; i < news.length; i++) {
+                    newSummaryTitles.push([news[i]["esDto"]["title"], news[i]["esDto"]["url"]])
+                }
+            }
+            else {
+                for (let i = 0; i < 2; i++) {
+                    newSummaryTitles.push([news[i]["esDto"]["title"], news[i]["esDto"]["url"]])
+                }
+            }
+            if (blog.length < 2) {
+                for (let i = 0; i < blog.length; i++) {
+                    newSummaryTitles.push([blog[i]["esDto"]["title"], blog[i]["esDto"]["url"]])
+                }
+            }
+            else {
+                for (let i = 0; i < 2; i++) {
+                    newSummaryTitles.push([blog[i]["esDto"]["title"], blog[i]["esDto"]["url"]])
+                }
+            }
+            setSummaryTitles(newSummaryTitles)
+        });
     }, [searchTerm]);
 
     return (
@@ -22,7 +49,7 @@ function SearchResultPage() {
                 <SearchResultNav searchWord={searchTerm} searchUrlPath={"search"} />
                 <div style={style.body}>
                     <SearchResultLeftBox searchResults={searchResults} />
-                    <SearchResultRightBox searchTerm={searchTerm} searchResults={searchResults} title={title} /> {/* title props 전달 */}
+                    <SearchResultRightBox searchTerm={searchTerm} searchResults={searchResults} summaryTitles = {summaryTitles}/> {/* title props 전달 */}
                 </div>
             </section>
         </div>
