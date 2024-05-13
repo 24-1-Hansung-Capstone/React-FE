9e57b4e852307dab2e2acb81e1c1940d0c0e44b6
lee <sjlee9908@naver.com>
2024-05-13 18:06:33 +0900

    dd
    dd

--- src/Component/SearchResultPage/RelatedSearchWord.jsx
+++ src/Component/SearchResultPage/RelatedSearchWord.jsx
@@ -1,10 +1,24 @@
 // src/Components/SearchResultPage/RelatedSearchWord.jsx
 import React from "react";
 import style from "./style/RelatedSearchWordStyle"
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function RelatedSearchWord({ recommendation }) {
+
+    const navigate = useNavigate();
+    
+    const url = `http://localhost:3000/search/${recommendation}`;
+
     return (
-        <div style={style.relatedSearchWord}>{recommendation}</div>
+        <a
+            style={{ textDecoration: 'none', color: 'black' }}
+            href = {url}
+        >
+            <div style={style.relatedSearchWord}>
+            {recommendation}
+            </div>
+        </a>
     );
 }
 
