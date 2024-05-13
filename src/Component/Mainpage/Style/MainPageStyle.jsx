c8fe88e6c9e69efc6c5875881c7223bec0e12ad4
pyyhekumi82 <pyyhekumi82@gmail.com>
2024-05-13 21:00:53 +0900

    Update MainPageStyle.jsx

--- src/Component/Mainpage/Style/MainPageStyle.jsx
+++ src/Component/Mainpage/Style/MainPageStyle.jsx
@@ -1,13 +1,15 @@
 const style = {
     logo: {
         width: '25%',
-        margin: '10% 0'
+        margin: '10% 0',
+        backgroundcolor : '#EBF7FF',
     },
     mainPageBody :{
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        backgroundcolor : '#EBF7FF',
     }
 
 
