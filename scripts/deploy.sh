0eaaeb819762f6494a3c198384f416f613681c68
jin3811 <66506964+jin3811@users.noreply.github.com>
2024-05-13 20:46:06 +0900

    개발서버보다 램을 많이 먹는 빌드

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,17 +9,11 @@ PID_FILE="$REPOSITORY/.pid"
 echo "> starting..."
 cd $REPOSITORY
 
-echo "> install dependency"
-npm install
-
 echo "> stopping previous server..."
 if [ -f $PID_FILE ]; then
     kill -9 `cat $PID_FILE`
     rm $PID_FILE
 fi
-
-echo "> build project log : [$BUILD_LOG]"
-npm run build > $BUILD_LOG
 
 echo "> run log : [$RUN_LOG]"
 nohup npx -s serve build > $RUN_LOG 2>&1 &
