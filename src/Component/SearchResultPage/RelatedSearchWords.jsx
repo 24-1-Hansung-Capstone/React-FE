94ddb98a69faaec04a8d3608254b0231d6adc1b1
lee <sjlee9908@naver.com>
2024-05-13 18:06:33 +0900

    dd
    dd

--- src/Component/SearchResultPage/RelatedSearchWords.jsx
+++ src/Component/SearchResultPage/RelatedSearchWords.jsx
@@ -3,11 +3,11 @@ import React from "react";
 import RelatedSearchWord from "./RelatedSearchWord";
 import style from "./style/RelatedSearchWordsStyle"
 
-function RelatedSearchWords({recommendations}){
+function RelatedSearchWords({recommendations }){
     return (
         <div style={style.relatedSearchWords}>
             {recommendations.map((recommendation, index) => (
-                <RelatedSearchWord key={index} recommendation={recommendation} />
+                <RelatedSearchWord key={index} recommendation={recommendation}/>
             ))}
         </div>
     )
