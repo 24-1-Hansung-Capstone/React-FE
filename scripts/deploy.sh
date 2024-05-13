b074b3bce5e2ddcca0061a3792f9db8a7621fbd2
jin3811 <66506964+jin3811@users.noreply.github.com>
2024-05-13 19:19:20 +0900

    배포 젭알

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,22 @@ PROJECTNAME=React-FE
 REPOSITORY=/home/ubuntu/$PROJECTNAME
 BUILD_LOG=$REPOSITORY/build.log
 RUN_LOG=$REPOSITORY/webapp.log
+PID_FILE="$REPOSITORY/.pid"
 
-cd $REPOSITORY
 echo "> starting..."
-# echo "> install dependency"
-# npm install
-# npm run build > $BUILD_LOG=$REPOSITORY
-# nohup npx -s serve build > $RUN_LOG 2>&1 &
+cd $REPOSITORY
+
+echo "> install dependency"
+npm install
+
+echo "> stopping previous server..."
+if [ -f $PID_FILE ]; then
+    kill -9 `cat $PID_FILE`
+    rm $PID_FILE
+fi
+
+echo "> build project log : [$BUILD_LOG]"
+npm run build > $BUILD_LOG
+
+echo "> run log : [$RUN_LOG]"
+nohup npx -s serve build > $RUN_LOG 2>&1 &
