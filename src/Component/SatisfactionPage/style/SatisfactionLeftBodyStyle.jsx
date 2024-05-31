261abadfd6fb4acde6893b4bcc7ef9df6fda1b48
jin3811 <hjwoo1028@naver.com>
2024-05-31 10:05:53 +0900

    ouyf

--- src/Component/SatisfactionPage/style/SatisfactionLeftBodyStyle.jsx
+++ src/Component/SatisfactionPage/style/SatisfactionLeftBodyStyle.jsx
@@ -2,7 +2,7 @@ const style = {
     leftBox : {
         //searchResultRightBox과 맞춰야함
         position: "sticky",
-        width : "70vh",
+        width : "90vh",
         margin: "4vh 5vh",
         marginLeft : "2vh",
         marginRight : "2vh",
